0e30fc11132330511c153b74b5c2fe1e10ee6593
Kirill Klenski <kirill.klenski@gmail.com>
2012-08-02 01:31:19 +0300

    Working version - basic requirements satisfied.

--- src/main/java/org/kklenski/ClassFinder.java
+++ src/main/java/org/kklenski/ClassFinder.java
@@ -4,12 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ClassFinder {
     
     private InputStream in;
+    
+    private MatchMode mode;
+    private char match;
+    //TODO: multiIndex
+    private int matchIdx;
+
+	private int multiIdx = -1;
 
     /**
      * @param in Class names on separate lines
@@ -19,37 +29,93 @@ public class ClassFinder {
     }
 
     public Collection<String> findMatching(String pattern) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		// we should expect unicode as class names can consist of unicode chars,
+		// see: http://stackoverflow.com/questions/1422655/java-unicode-variable-names
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8"))); 
         ArrayList<String> result = new ArrayList<String>();
         try {
             String className;
             while ((className = reader.readLine()) != null) {
-                if (matches(className, pattern)) {
+                if (matches(getSimpleName(className), pattern)) {
                     result.add(className);
                 }
             }
         } catch (IOException e) {
             //FIXME: what should we do? throw exception, return empty list or current findings
         }
+        
+        Collections.sort(result, new Comparator<String>() {
+			@Override
+			public int compare(String className1, String className2) {
+				return getSimpleName(className1).compareTo(getSimpleName(className2));
+			}
+		});
+        
         return result;
     }
+    
+    static String getSimpleName(String className) {
+    	//FIXME: test class with no package
+    	return className.substring(className.lastIndexOf('.')+1);
+	}
 
-    private boolean matches(String className, String pattern) {
-        int idx = 0;
-        for (int i = 0; i < pattern.length(); i++) {
-            char p = pattern.charAt(i);
-            switch (p) {
-            case '*':
-                break;
-            case ' ':
-                return idx == pattern.length()-1;
-            default:
-                boolean upperCase = Character.isUpperCase(p);
-                break;
-            }
+	private enum MatchMode {
+    	UPPER,
+    	LOWER,
+    	ZERO_OR_MORE,
+    	END
+    }
+
+	private void updateMode(String pattern) {
+		match = pattern.charAt(matchIdx);
+        switch (match) {
+        case ' ':
+            mode = MatchMode.END;
+            break;
+        case '*':
+        	match = pattern.charAt(++matchIdx);
+        	multiIdx = matchIdx;
+        default:
+            mode = Character.isUpperCase(match) ? MatchMode.UPPER : MatchMode.LOWER;
+            break;
         }
+        match = Character.toUpperCase(match);
+	}
+
+	//FIXME: empty pattern case test
+	//FIXME: Character."supplementary characters"
+    private boolean matches(String className, String pattern) {
+    	matchIdx = 0;
+        updateMode(pattern);
         
-        return false;
+        for (int i = 0; i < className.length(); i++) {
+        	char c = Character.toUpperCase(className.charAt(i));
+        	switch (mode) {
+        	case LOWER:
+        		if (c != match) {
+        			if (multiIdx != -1) {
+        				matchIdx = multiIdx;
+        				updateMode(pattern);
+        				multiIdx = -1;
+        			} else {
+        				return false;
+        			}
+        		}
+			case UPPER:
+				if (c == match) {
+					if (++matchIdx < pattern.length()) {
+						updateMode(pattern);
+						break;
+					} else {
+						return true;
+					}
+				}
+				break;
+			case END:
+				return false;
+        	}
+        }
+        return mode == MatchMode.END;
     }
 
 }
--- src/test/java/org/kklenski/ClassFinderTest.java
+++ src/test/java/org/kklenski/ClassFinderTest.java
@@ -1,13 +1,13 @@
 package org.kklenski;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -15,17 +15,20 @@ public class ClassFinderTest {
     
     @Test
     public void testBasicCases() {
-        String[] classNames = new String[] {"a.b.FooBarBaz", "c.d.FooBar"};
-        assertTrue(Arrays.equals(classNames, findMatching(classNames, "FB")));
-        assertTrue(Arrays.equals(classNames, findMatching(classNames, "FoBa")));
-        assertTrue(Arrays.equals(classNames, findMatching(classNames, "FBar")));
+        String[] classNames = new String[] {"c.d.FooBar", "a.b.FooBarBaz"};
+        
+        assertArrayEquals(classNames, findMatching(classNames, "FB"));
+        assertArrayEquals(classNames, findMatching(classNames, "FoBa"));
+        assertArrayEquals(classNames, findMatching(classNames, "FBar"));
     }
+    
+    //FIXME: add negative cases
     
     @Test
     public void testSpace() throws Exception {
         String[] classNames = new String[] {"a.b.FooBarBaz", "c.d.FooBar"};
         String[] expected = new String[] {"c.d.FooBar"};
-        assertTrue(Arrays.equals(expected, findMatching(classNames, "FBar ")));
+        assertArrayEquals(expected, findMatching(classNames, "FBar "));
         
         //FIXME: test space in other pattern places
     }
@@ -34,8 +37,8 @@ public class ClassFinderTest {
     public void testWildcard() throws Exception {
         String[] classNames = new String[] {"a.b.FooBarBaz", "c.d.FooBar"};
         String[] expected = new String[] {"a.b.FooBarBaz"};
-        assertTrue(Arrays.equals(expected, findMatching(classNames, "F*Baz")));
-        assertTrue(Arrays.equals(expected, findMatching(classNames, "*BB")));
+        assertArrayEquals(expected, findMatching(classNames, "F*Baz"));
+        assertArrayEquals(expected, findMatching(classNames, "*BB"));
         
         //FIXME: test plain wildcard / wildcard and space combinations
     }
@@ -44,11 +47,17 @@ public class ClassFinderTest {
     public void testSorting() throws Exception {
         String[] classNames = new String[] {"a.b.FooBarDaz", "c.d.FooBarBaz", "e.f.FooBarCaz"};
         String[] expected = new String[] {"c.d.FooBarBaz", "e.f.FooBarCaz", "a.b.FooBarDaz"};
-        assertTrue(Arrays.equals(expected, findMatching(classNames, "FB")));
+        assertArrayEquals(expected, findMatching(classNames, "FB"));
     }
     
-    //FIXME: test executing findMatching more than one time
+    @Test
+    public void testGetSimpleName() {
+    	assertEquals("FooBar", ClassFinder.getSimpleName("a.b.FooBar"));
+    	assertEquals("FooBar", ClassFinder.getSimpleName("FooBar"));
+    	assertEquals("", ClassFinder.getSimpleName("a.")); //FIXME: is it valid case?
+	}
     
+    //FIXME: test executing findMatching more than one time
     private String[] findMatching(String[] classNames, String pattern) {
         InputStream in = createStream(classNames);
         try {
@@ -64,9 +73,9 @@ public class ClassFinderTest {
     private InputStream createStream(String... classNames) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
-            PrintWriter writer = new PrintWriter(out);
+            PrintWriter writer = new PrintWriter(out, true);
             for (String className : classNames) {
-                writer.write(className);
+                writer.println(className);
             }
             return new ByteArrayInputStream(out.toByteArray());
         } finally {
